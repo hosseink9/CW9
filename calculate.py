8f62ff3916d92a3323439fc24687c47c0a6fc15e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-06-02 16:26:57 +0330

    calculate file

---
+++ calculate.py
@@ -0,0 +1,28 @@
+from operation.division import div
+from operation.addition import addition
+from operation.multi import multi
+from operation.subtraction import sub
+
+def cal(a,b,c):
+                
+    if b=="+" :
+        addition(a,c)
+    elif b=="-" :
+        sub(a,c)
+    elif b=="*":
+        multi(a,c)
+    elif b=="/" :
+        div(a,c)
+    else:
+        print("your input is wrong please try again")
+        
+
+print("Welcome")
+while True:
+    number1=int(input('Please enter your number one\nExit (e)\n'))
+    if operation=="e":
+        break
+    operation=input("Please enter your operation (+,-,*,/)\n")
+    number2=int(input('Please enter your number two\n'))
+    cal(number1,operation,number2)
+    
---
+++ operation/multi.py
@@ -0,0 +1,2 @@
+def multi(num1,num2):
+    print(num1*num2)
